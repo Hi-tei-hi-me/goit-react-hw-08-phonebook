e9e7a38a131a86ea382851088eec223470f79ad9
Hi-tei-hi-me <104867722+Hi-tei-hi-me@users.noreply.github.com>
2023-01-28 16:11:52 +0000

    Deploying to gh-pages from @ Hi-tei-hi-me/goit-react-hw-08-phonebook@5be9d01b3100eea3ca3887deaf4dd6db034c01bf 🚀

---
+++ static/js/946.bfc013ea.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[946],{6946:function(e,o,n){n.r(o),n.d(o,{default:function(){return s}});var r,t=n(168),i=n(7402).Z.div(r||(r=(0,t.Z)(["\n  margin-top: 20px;\n"]))),h=n(184);function s(){return(0,h.jsxs)(i,{children:[(0,h.jsx)("h1",{children:"Welcome to your personal Phonebook!"}),(0,h.jsx)("h2",{children:"Please login or register to use it"})]})}}}]);
+//# sourceMappingURL=946.bfc013ea.chunk.js.map
